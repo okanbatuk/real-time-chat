c3a10e77f873324dc8e8bbd6dc0505a08f6d9ce3
myrnz <okanbatuk@protonmail.com>
2022-08-18 21:34:05 +0300

    Server configuration and build

---
+++ server/src/config/express.js
@@ -0,0 +1,16 @@
+const express = require("express"),
+  morgan = require("morgan"),
+  bodyParser = require("body-parser");
+
+const app = express();
+
+// Request logging level
+app.use(morgan("dev"));
+
+// Parse body params and attach them to req.body
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Routes is here
+
+module.exports = app;
---
+++ server/src/server.js
@@ -0,0 +1,11 @@
+const express = require("express"),
+  http = require("http"),
+  app = require("./config/express.js"),
+  { host, port } = require("./config/vars");
+
+// Server configuration and build
+const server = http.createServer(app);
+server.listen(port, (err) => {
+  if (err) throw err;
+  console.log(`Server running at http://${host}:${port}`);
+});
